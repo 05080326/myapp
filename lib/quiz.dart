285ab40b10602f392b79da6a7d2645b63d3f029f
Joni <jdvalenccia@diwamail.com>
2025-11-22 13:09:26 +0800

    final

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,6 +22,15 @@ var activeScreen = 'start-screen';
 
   @override
   Widget build(context) {
+    //** todo Task 2-1 */
+   Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionScreen();
+    }  
+
+
+    //** End Task 2-1 */
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -35,9 +44,14 @@ var activeScreen = 'start-screen';
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen' 
-            ? StartScreen(switchScreen) 
-            : const QuestionScreen(),
+          // child: activeScreen == 'start-screen' 
+          //   ? StartScreen(switchScreen) 
+          //   : const QuestionScreen(),
+
+          //** todo Task 2-2 */
+          child: screenWidget,
+           //** End Task 2-2 */
+          
         ),
       ),
     );
